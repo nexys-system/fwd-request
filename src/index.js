f8cc62b9642828d8457d852b454da1dcf83c650b
Johan <johan@macair2020>
2023-02-17 10:10:29 +0100

    port as env var

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const { TARGET : target } = process.env;
+const { TARGET : target, PORT } = process.env;
 
 if (!target) {
   throw Error('target must be set');
 }
 
+const port = PORT ? Number(PORT) : 3000;
+
 const app = express();
 app.use('*', createProxyMiddleware({target, changeOrigin: true}));
-app.listen(3000);
+app.listen(port);
